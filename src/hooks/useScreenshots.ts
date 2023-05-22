d8a8ea99289b99286e2fb0e3875239c987899e83
DijanaVN <veljanovskadijana@yahoo.com>
2023-05-22 12:03:58 +0200

    Build Game Screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+import { Screenshots } from "./../entities/Screenshots";
+
+const useScreenshots = (game_pk: string | number) => {
+  const apiClient = new APIClient<Screenshots>(`/games/${game_pk}/screenshots`);
+  return useQuery({
+    queryKey: ["screenshots", game_pk],
+    queryFn: apiClient.getAll,
+  });
+};
+export default useScreenshots;
